a61c9e286973f75e9fb5b91b39536f059b9508f6
RYOMA <naganoryoma@icloud.com>
2026-01-20 22:11:48 -0500

    finished 04

--- app/src/main/java/edu/duke/ece651/rn176/graph/Location.java
+++ app/src/main/java/edu/duke/ece651/rn176/graph/Location.java
@@ -1,28 +1,29 @@
 package edu.duke.ece651.rn176.graph;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Location {
   private final String name;
-  private final List<Edge> edges;
+  private final Map<Location, Edge> edges;
 
   public Location(String name) {
     this.name = name;
-    this.edges = new ArrayList<>();
+    this.edges = new HashMap<>();
   }
 
   public Iterable<Edge> getEdges() {
-    return edges;
+    return edges.values();
   }
 
   public void addEdge(Edge e) {
-    for (Edge e2 : edges) {
-      if (e.getDestination().equals(e2.getDestination())) {
+    Location dst = e.getDestination();
+    if(edges.get(dst) != null){
         throw new IllegalArgumentException("Attempt to add duplicate edge from " + this + " to " + e.getDestination());
       }
-      edges.add(e);
-    }
+    edges.put(dst,e);
   }
 
   public String getName() {
--- app/src/test/java/edu/duke/ece651/rn176/graph/LocationTest.java
+++ app/src/test/java/edu/duke/ece651/rn176/graph/LocationTest.java
@@ -1,27 +1,63 @@
 package edu.duke.ece651.rn176.graph;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
+
 public class LocationTest {
+  Location rohan = new Location("Rohan");
+  Location mordor = new Location("Mordor");
+  Location gondor = new Location("Gondor");
   @Test
   public void test_getName() {
-    Location testLoc = new Location("Gondor");
-    assertEquals("Gondor", testLoc.getName());
+    assertEquals("Gondor", gondor.getName());
   }
 
   @Test
   public void test_toString() {
-    Location testLocation = new Location("Mordor");
-    assertEquals("Mordor", testLocation.toString());
+    assertEquals("Mordor", mordor.toString());
+  }
+
+  private void checkEdgeIterable(Iterable<Edge> it, Edge... expected) {
+    Set<Edge> expectedSet = new HashSet<>();
+    for(Edge e: expected) {
+      expectedSet.add(e);
+    }
+    for(Edge e: it) {
+      assertTrue(expectedSet.contains(e));
+      expectedSet.remove(e);
+    }
+    assertTrue(expectedSet.isEmpty());
   }
 
   @Test
   public void test_getEdges() {
-    Location rohan = new Location("Rohan");
-    assertEquals(new ArrayList<>(), rohan.getEdges());
+    checkEdgeIterable(rohan.getEdges());
+    Edge e1 = new Edge(mordor, 99);
+    Edge e2 = new Edge(rohan, 37);
+    Edge e3 = new Edge(gondor, 37);
+    Edge e4 = new Edge(gondor,99);
+    rohan.addEdge(e3);
+    gondor.addEdge(e1);
+    gondor.addEdge(e2);
+    mordor.addEdge(e4);
+    checkEdgeIterable(rohan.getEdges(), e3);
+    checkEdgeIterable(gondor.getEdges(), e1, e2);
+    checkEdgeIterable(gondor.getEdges(), e2, e1);
+    checkEdgeIterable(mordor.getEdges(), e4);
+  }
+  @Test
+  public void test_duplicate_dest() {
+    rohan.addEdge(new Edge(gondor, 12));
+    assertThrows(IllegalArgumentException.class, ()->rohan.addEdge(new Edge(gondor, 57)));
+
   }
 }
