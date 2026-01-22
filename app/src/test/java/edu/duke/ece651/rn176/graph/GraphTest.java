39f48e271eb9694d504bf363e9f9f5c8a39442b1
RYOMA <naganoryoma@icloud.com>
2026-01-21 20:00:02 -0500

    finished 12

--- app/src/test/java/edu/duke/ece651/rn176/graph/GraphTest.java
+++ app/src/test/java/edu/duke/ece651/rn176/graph/GraphTest.java
@@ -3,6 +3,12 @@ package edu.duke.ece651.rn176.graph;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class GraphTest {
     @Test
     public void test_getOrCreate(){
@@ -51,4 +57,59 @@ public class GraphTest {
         Location roshar =  g.getOrCreate("Roshar", true);
         TestUtils.checkIterableWithoutOrder(g.getLocations(), mordor, gondor, shire, scadrial, roshar);
     }
+    @Test
+    public void test_toString(){
+        Graph g = new Graph();
+        assertEquals("<>", g.toString());
+        Location mordor = g.getOrCreate("Mordor", true);
+        assertEquals("<Mordor>", g.toString());
+        Location gondor = g.getOrCreate("Gondor", true);
+        Location shire = g.getOrCreate("Shire", true);
+        Location scadrial = g.getOrCreate("Scadrial",  true);
+        Location roshar = g.getOrCreate("Roshar", true);
+        String expected = "<Gondor,Mordor,Roshar,Scadrial,Shire>";
+        assertEquals(expected, g.toString());
+    }
+
+    private Graph readGraphFromFile(String name) throws IOException {
+        InputStream st = getClass().getClassLoader().getResourceAsStream(name);
+        InputStreamReader reader = new InputStreamReader(st);
+        GraphReader gr = new GraphReader(reader);
+        Graph g = gr.readGraph();
+        return g;
+    }
+    private void checkShortestPath(Graph g, String startName, String endName, int expectedCost, String... expectedNames) {
+        Location start = g.getOrCreate(startName, false);
+        Location end = g.getOrCreate(endName, false);
+        Path p = g.shortestPath(start, end);
+
+        if (expectedNames == null) {
+            assertNull(p);
+            return;
+        }
+
+        assertNotNull(p);
+        assertEquals(expectedCost, p.getTotalWeight());
+        assertEquals(startName, p.getStart().getName());
+
+        Iterable<String> nodeNames = TestUtils.transformIterable(p.getEdges(), (e) -> e.getDestination().getName());
+        ArrayList<String> nameList = new ArrayList<>();
+        Collections.addAll(nameList, expectedNames);
+        assertIterableEquals(nameList, nodeNames);
+    }
+    @Test
+    public void test_shortestPath1() throws IOException {
+        Graph g = readGraphFromFile("graphs/graph1.txt");
+        checkShortestPath(g, "Durham", "Durham", 0);
+        checkShortestPath(g, "Durham", "Montreal", 778, "Philadelphia", "Montreal");
+        checkShortestPath(g, "Dallas", "Philadelphia", 1397, "Louisville", "Philadelphia");
+    }
+    @Test
+    public void test_shortestPath2() throws IOException {
+        Graph g = readGraphFromFile("graphs/graph2.txt");
+        checkShortestPath(g, "D", "A", 0, (String[]) null);
+        checkShortestPath(g, "A", "F", 17, "B", "C", "E", "F");
+    }
+
+
 }
